ae8508348c04a4aab2bf85e0bc387844b3fff774
“reddrum” <anton.kranz@me.com>
2016-10-06 21:36:18 +0300

    Add show/edit/delete pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,11 @@
 class PinsController < ApplicationController
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
   def index
+    @pins = Pin.all.order("created_at DESC")
+  end
+
+  def show
 
   end
 
@@ -17,9 +23,30 @@ class PinsController < ApplicationController
     end
   end
 
+  def edit
+
+  end
+
+  def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: "Pin was successfully updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pin.destroy
+    redirect_to root_path
+  end
+
   private
 
   def pin_params
     params.require(:pin).permit(:title, :description)
+  end
+
+  def find_pin
+    @pin = Pin.find(params[:id])
   end
 end
